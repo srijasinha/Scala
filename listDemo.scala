f91b28d9011b3de18347faf93694dc9d1aa8da9c
Srija Sinha <161572666+srijasinha@users.noreply.github.com>
2025-04-05 19:23:27 +0530

    Add files via upload

--- listDemo.scala
+++ listDemo.scala
@@ -5,9 +5,27 @@ Object ListDemo
         var fruit :List[String] =List =("Apple","Banana","orange")
         var nums:List[Int] = List(1,3,3,44)
         var dim: List[List[Int]] =List(list(1,2,3,4),list(3,4,5,2),list(3,3,66,34))
-        var frit ="apple" ::("orange"::("param" :: Nil))
+        var fruit ="apple" ::("orange"::("param" :: Nil))
         println("head"+ fruit.head)
         println("tail"+ nums.tail)
         println("empty" +dim.isEmpty)
+    }
+}
+
+//More on Lists
+
+bject ListDemo
+{
+    def main(args: Array[string]):Unit =
+    {
+        var fruit = "Apple" :: ("Banana" :: ("param" :: Nil))
+        println("reverse fruit" + fruit.reverse)
+        var fruit1 ="mangoes" ::("banana":: Nil)
+        var vijay = fruit::: fruit1
+        println(" vijay" +vijay)
+        var ajay = fruit.::: (fruit1)
+        println("ajay" +ajay)
+        var srija - List.concat(fruit,fruit1)
+        println("List" + srija)
     }
 }
