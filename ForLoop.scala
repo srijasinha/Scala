f274e8232b68b30875d60f98d42074b4c12b8aba
Srija Sinha <161572666+srijasinha@users.noreply.github.com>
2025-04-05 13:19:01 +0530

    Add files via upload

--- ForLoop.scala
+++ ForLoop.scala
@@ -1,3 +1,4 @@
+//For Loop
 Object FoorLoop
 {
     def main(args: Array[string]):Unit =
@@ -7,5 +8,20 @@ Object FoorLoop
             for (a<-1 to 3,b-<1 to 9)
                 println("value of a is"+a)
                 println("value of b is"+b)
+    }
+}
+
+//For Loop List 
+Object FoorLoop1
+{
+    def main(args: Array[string]):Unit =
+    {
+        var a = 0 
+        Var numList = List(1,2,3,4,5,6,7)
+         for (a<-numList
+             if(a != 3;if a < 8 ))
+         {
+            println("value of a is"+a)
+         }
     }
 }
