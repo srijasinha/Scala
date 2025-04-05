52876b7c0b63365cc202cd4d6ef728570033c33a
Srija Sinha <161572666+srijasinha@users.noreply.github.com>
2025-04-05 14:10:06 +0530

    Add files via upload

---
+++ Break.scala
@@ -0,0 +1,34 @@
+Object DoWhileLoop
+{
+    def main(args: Array[string]):Unit =
+    {
+        var a = 0
+        var b = 0
+        var numList =  List(1,3,4,5)
+        var numlist1 =List(43,44,53,4)
+        var loop = new Break 
+        var inner = new Breaks
+        loop.breakable 
+        {
+            for(a <-numList)
+            {
+                println("The value of a is" +a)
+
+                inner.breakable{
+                    for(b<-numlist1)
+                    {
+                        println("The value of a is" +b)
+                    }
+                    if (b == 4)
+                {
+                   inner.break;
+                }            
+            }
+            println("This is inner break")
+            
+        }
+        println("After the loop")
+    }
+}
+
+    
