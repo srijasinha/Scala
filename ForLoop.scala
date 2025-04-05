da9eba1487252abf51e9f0952cf6c477a2bf5bdb
Srija Sinha <161572666+srijasinha@users.noreply.github.com>
2025-04-05 12:54:25 +0530

    Add files via upload

--- ForLoop.scala
+++ ForLoop.scala
@@ -3,7 +3,9 @@ Object FoorLoop
     def main(args: Array[string]):Unit =
     {
         var a = 0 
-            for (a<-1 to 10)
+        var b = 0 
+            for (a<-1 to 3,b-<1 to 9)
                 println("value of a is"+a)
+                println("value of b is"+b)
     }
 }
